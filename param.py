853b4d445cc09e642dff72c366dacf2ea7494567
LiangWenshuo1118 <73432685+LiangWenshuo1118@users.noreply.github.com>
2022-05-05 10:46:51 +0800

    Update param.py

--- param.py
+++ param.py
@@ -1,6 +1,7 @@
 from context import dargs
 from dargs import dargs, Argument, 
 
+# basics
 def type_map_args ():
     doc_type_map = 'Atom types.'
     
@@ -24,4 +25,80 @@ def use_ele_temp_args ():
     return [
         Argument("use_ele_temp", int, optional = False, default = 0, doc = doc_use_ele_temp)
     ]
+  
+from context import dargs
+from dargs import dargs, Argument, 
+
+def type_map_args ():
+    doc_type_map = 'Atom types.'
+    
+    return [
+        Argument("type_map", List of string, optional = False, doc = doc_type_map)
+    ]
+  
+def mass_map_args ():
+    doc_mass_map = 'Standard atom weights.'
+    
+    return [
+        Argument("mass_map", List of float, optional = False, doc = doc_mass_map)
+    ]
+
+# Data
+def init_data_prefix_args ():
+    doc_init_data_prefix = 'Prefix of initial data directories.'
+    
+    return [
+        Argument("init_data_prefix", String, optional = True, doc = doc_init_data_prefix)
+    ]
+
+def init_data_sys_args ():
+    doc_init_data_sys = 'Directories of initial data. You may use either absolute or relative path here.'
+    
+    return [
+        Argument("init_data_prefix", List of string, optional = False, doc = doc_init_data_sys)
+    ]
+
+def sys_format_args ():
+    doc_sys_format = 'Format of initial data. It will be vasp/poscar if not set.'
+    
+    return [
+        Argument("sys_format", String, optional = False, doc = doc_sys_format)
+    ]
+
+def init_multi_systems_args ():
+    doc_init_multi_systems = 'If set to true, init_data_sys directories should contain sub-directories of various systems. DP-GEN will regard all of these sub-directories as inital data systems.'
+    
+    return [
+        Argument("init_multi_systems", Boolean, optional = True, doc = doc_init_multi_systems)
+    ]
+
+def init_batch_size_args ():
+    doc_init_batch_size = 'Each number is the batch_size of corresponding system for training in init_data_sys. One recommended rule for setting the sys_batch_size and init_batch_size is that batch_size mutiply number of atoms ot the stucture should be larger than 32. If set to auto, batch size will be 32 divided by number of atoms.'
+    
+    return [
+        Argument("init_batch_size", String of integer, optional = True, doc = doc_init_batch_size)
+    ]
+
+def sys_configs_prefix_args ():
+    doc_sys_configs_prefix = 'Prefix of sys_configs.'
+    
+    return [
+        Argument("sys_configs_prefix", String, optional = True, doc = doc_sys_configs_prefix)
+    ]
+
+def sys_configs_args ():
+    doc_sys_configs = 'Containing directories of structures to be explored in iterations.Wildcard characters are supported here.'
+    
+    return [
+        Argument("sys_configs", String, optional = False, doc = doc_sys_configs)
+    ]
+
+def sys_batch_size_args ():
+    doc_sys_batch_size = 'Each number is the batch_size for training of corresponding system in sys_configs. If set to auto, batch size will be 32 divided by number of atoms.'
+    
+    return [
+        Argument("sys_batch_size", List of integer, optional = True, doc = doc_sys_batch_size)
+    ]
+
+#Training
   
