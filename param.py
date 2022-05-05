0c74c57238b2ff8335223c4fe9821b97fdb6d1f5
LiangWenshuo1118 <73432685+LiangWenshuo1118@users.noreply.github.com>
2022-05-05 11:05:55 +0800

    Update param.py

--- param.py
+++ param.py
@@ -100,5 +100,84 @@ def sys_batch_size_args ():
         Argument("sys_batch_size", List of integer, optional = True, doc = doc_sys_batch_size)
     ]
 
-#Training
+# Training
+def numb_models_args ():
+    doc_numb_models = 'Number of models to be trained in 00.train. 4 is recommend.'
+    
+    return [
+        Argument("numb_models", Integer, optional =False, doc = doc_numb_models)
+    ]
+
+def training_iter0_model_path_args ():
+    doc_training_iter0_model_path = 'The model used to init the first iter training. Number of element should be equal to numb_models.'
+    
+    return [
+        Argument("training_iter0_model_path", list of string, optional =True, doc = doc_training_iter0_model_path)
+    ]
   
+
+def training_init_model_args ():
+    doc_training_init_model = 'Iteration > 0, the model parameters will be initilized from the model trained at the previous iteration. Iteration == 0, the model parameters will be initialized from training_iter0_model_path.'
+    
+    return [
+        Argument("training_init_model", bool, optional =True, doc = doc_training_init_model)
+    ]
+
+def default_training_param_args ():
+    doc_default_training_param = 'Training parameters for deepmd-kit in 00.train. You can find instructions from here: (https://github.com/deepmodeling/deepmd-kit).'
+    
+    return [
+        Argument("default_training_param", Dict, optional =False, doc = doc_default_training_param)
+    ]
+
+def dp_compress_args ():
+    doc_dp_compress = 'Use dp compress to compress the model.'
+    
+    return [
+        Argument("dp_compress", bool, optional =True, default = false, doc = doc_dp_compress)
+    ]
+
+# Exploration
+def model_devi_dt_args ():
+    doc_model_devi_dt = 'Timestep for MD. 0.002 is recommend.'
+    
+    return [
+        Argument("model_devi_dt", Float, optional =False, doc = doc_model_devi_dt)
+    ]
+		
+def model_devi_skip_args ():
+    doc_model_devi_skip = 'Number of structures skipped for fp in each MD.'
+    
+    return [
+        Argument("model_devi_skip", Integer, optional =False, doc = doc_model_devi_skip)
+    ]	
+
+def model_devi_f_trust_lo_args ():
+    doc_model_devi_f_trust_lo = 'Lower bound of forces for the selection. If List, should be set for each index in sys_configs, respectively.'
+    
+    return [
+        Argument("model_devi_f_trust_lo", Float or List of float, optional =False, doc = doc_model_devi_f_trust_lo)
+    ]
+
+def model_devi_f_trust_hi_args ():
+    doc_model_devi_f_trust_hi = 'Upper bound of forces for the selection. If List, should be set for each index in sys_configs, respectively.'
+    
+    return [
+        Argument("model_devi_f_trust_hi", Float or List of float, optional =False, doc = doc_model_devi_f_trust_hi)
+    ]
+
+def model_devi_v_trust_lo_args ():
+    doc_model_devi_v_trust_lo = 'Lower bound of virial for the selection. If List, should be set for each index in sys_configs, respectively. Should be used with DeePMD-kit v2.x.'
+    
+    return [
+        Argument("model_devi_v_trust_lo", Float or List of float, optional =False, doc = doc_model_devi_v_trust_lo)
+    ]
+
+def model_devi_v_trust_hi_args ():
+    doc_model_devi_v_trust_hi = 'Upper bound of virial for the selection. If List, should be set for each index in sys_configs, respectively. Should be used with DeePMD-kit v2.x.'
+    
+    return [
+        Argument("model_devi_v_trust_hi", Float or List of float, optional =False, doc = doc_model_devi_v_trust_hi)
+    ]
+
+model_devi_adapt_trust_lo	Boolean	False	Adaptively determines the lower trust levels of force and virial. This option should be used together with model_devi_numb_candi_f, model_devi_numb_candi_v and optionally with model_devi_perc_candi_f and model_devi_perc_candi_v. dpgen will make two sets: 1. From the frames with force model deviation lower than model_devi_f_trust_hi, select max(model_devi_numb_candi_f, model_devi_perc_candi_f*n_frames) frames with largest force model deviation. 2. From the frames with virial model deviation lower than model_devi_v_trust_hi, select max(model_devi_numb_candi_v, model_devi_perc_candi_v*n_frames) frames with largest virial model deviation. The union of the two sets is made as candidate dataset
